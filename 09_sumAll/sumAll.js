e14f255e91eff4f6e1f8e1abdd45ce262451f006
ron <lucifron28@gmail.com>
2025-06-02 21:48:37 +0800

    submit exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(x, y) {
+  let sum = 0;
+  for (let i = x; i <= y; i++) {
+    sum += i;
+  }
+  return sum;
 };
+console.log(sumAll(1, 10));
 
 // Do not edit below this line
 module.exports = sumAll;
